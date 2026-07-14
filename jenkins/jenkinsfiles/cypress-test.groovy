e1946ca41c885195a458ecb913ce1187dbe62996
Ahmad Wahbi <139769584+AhmadSealights@users.noreply.github.com>
2026-07-14 17:10:41 +0300

    Initial commit

---
+++ jenkins/jenkinsfiles/cypress-test.groovy
@@ -0,0 +1,63 @@
+pipeline {
+  agent {
+    kubernetes {
+      yaml readTrusted('jenkins/pod-templates/cypress.yaml')
+      defaultContainer "shell"
+    }
+  }
+
+    parameters {
+        string(name: 'BRANCH', defaultValue: 'public', description: 'Branch to clone (ahmad-branch)')
+        string(name: 'SL_LABID', defaultValue: '', description: 'Lab_id')
+    }
+    environment {
+      NO_COLOR = "true"
+      MACHINE_DNS = 'http://internal-template.btq.sealights.co:8081'
+    }
+    options{
+        buildDiscarder logRotator(numToKeepStr: '10')
+        timestamps()
+    }
+
+    stages{
+        stage("Init test"){
+            steps{
+                script{
+                git branch: params.BRANCH, url: 'https://github.com/Sealights-btq/template-btq.git'
+                }
+            }
+        }
+        stage("Setup pnpm") {
+          steps {
+            script {
+              sh """
+                corepack enable || true
+                corepack prepare pnpm@latest --activate || npm install -g pnpm
+              """
+            }
+          }
+        }
+
+        stage('download NodeJs agent and scanning Cypress tests') {
+            steps{
+                script{
+                    withCredentials([string(credentialsId: 'sealights-token', variable: 'SL_TOKEN')]) {
+                        sh """
+                        cd integration-tests/cypress/
+                        pnpm install
+                        pnpm install sealights-cypress-plugin
+                        pnpm exec cypress install
+                        export NODE_DEBUG=sl
+                        export CYPRESS_SL_ENABLE_REMOTE_AGENT=true
+                        export CYPRESS_SL_TEST_STAGE="Cypress-Test-Stage"
+                        export CYPRESS_machine_dns="${env.MACHINE_DNS}"
+                        export CYPRESS_SL_LAB_ID="${params.SL_LABID}"
+                        export CYPRESS_SL_TOKEN="${env.SL_TOKEN}"
+                        pnpm exec cypress run
+                        """
+                    }
+                }
+            }
+        }
+    }
+}
